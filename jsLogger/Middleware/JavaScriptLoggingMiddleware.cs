13100bb694ef1982d9d235c3030a8da702397ec9
hishamco <hishamco_2007@yahoo.com>
2017-02-19 03:07:27 +0300

    Cleanup

--- jsLogger/Middleware/JavaScriptLoggingMiddleware.cs
+++ jsLogger/Middleware/JavaScriptLoggingMiddleware.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
